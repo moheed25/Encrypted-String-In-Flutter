a1d23b7cd2fa58562d008035026c51967a04ab53
moheed25 <mohedaziz123@gmail.com>
2023-05-03 16:57:56 +0500

    complete

---
+++ encrypstr/android/app/src/main/kotlin/com/example/encrypstr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.encrypstr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ encrypstr/lib/HomePage.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_string_encryption/flutter_string_encryption.dart';
+
+class Home extends StatefulWidget {
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController controller = TextEditingController();
+
+  String encryptedtext = 'Encrypted Text';
+
+  String decryptedtext = 'Decrypted Text';
+
+  late PlatformStringCryptor cryptor;
+
+  String final_key = '';
+
+  void encrypt() async {
+    cryptor = PlatformStringCryptor();
+    final salt = await cryptor.generateSalt();
+    String key = await cryptor.generateKeyFromPassword(controller.text, salt);
+    String encrypted = await cryptor.encrypt(controller.text, key);
+
+    setState(() {
+      final_key = key;
+      encryptedtext = encrypted;
+    });
+  }
+
+  void decrypt() async {
+    try {
+      String decrypted = await cryptor.decrypt(encryptedtext, final_key);
+      setState(() {
+        decryptedtext = decrypted;
+      });
+    } on MacMismatchException {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Encryption')),
+        body: SingleChildScrollView(
+          child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: controller,
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(
+                      onPressed: () {
+                        encrypt();
+                      },
+                      child: Container(
+                          color: Colors.blue,
+                          width: 300,
+                          height: 50,
+                          child: Center(
+                              child: Text(
+                            'Encrypt',
+                            style: TextStyle(color: Colors.white),
+                          )))),
+                  SizedBox(height: 20),
+                  Text(
+                    encryptedtext,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(
+                      onPressed: () {
+                        decrypt();
+                      },
+                      child: Container(
+                          color: Colors.blue,
+                          width: 300,
+                          height: 50,
+                          child: Center(
+                              child: Text(
+                            'Decrypt',
+                            style: TextStyle(color: Colors.white),
+                          )))),
+                  SizedBox(height: 20),
+                  Text(
+                    decryptedtext,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              )),
+        ));
+  }
+}
